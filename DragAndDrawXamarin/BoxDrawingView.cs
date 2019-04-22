da7fd5b7bd2833f5f3ab71977f959f45f0946581
Ioana-Paula Stroe <stroe.ioanapaula@gmail.com>
2019-04-22 11:27:48 +0300

    Chapter-31: Custom Views and Touch Events - Theory

---
+++ DragAndDrawXamarin/BoxDrawingView.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using DragAndDrawXamarin.Models;
+
+namespace DragAndDrawXamarin
+{
+    public class BoxDrawingView : View
+    {
+        private new const string Tag = "BoxDrawingView";
+
+        private Box _currentBox;
+        private List<Box> _boxes = new List<Box>();
+        private Paint _boxPaint;
+        private Paint _backgroundPaint;
+
+        public BoxDrawingView(Context context) : this(context, null)
+        {
+            Initialize();
+        }
+
+        public BoxDrawingView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            var current = new PointF(e.GetX(), e.GetY());
+            string action = string.Empty;
+
+            switch (e.Action)
+            {
+                case MotionEventActions.Down:
+                    action = "ActionDown";
+                    _currentBox = new Box(current);
+                    _boxes.Add(_currentBox);
+
+                    break;
+                case MotionEventActions.Move:
+                    action = "ActionMove";
+
+                    if (_currentBox != null)
+                    {
+                        _currentBox.Current = current;
+                        Invalidate();
+                    }
+
+                    break;
+                case MotionEventActions.Up:
+                    action = "ActionUp";
+                    _currentBox = null;
+
+                    break;
+                case MotionEventActions.Cancel:
+                    action = "ActionCancel";
+                    _currentBox = null;
+
+                    break;
+            }
+
+            Log.Info(Tag, $"{action} at x = {current.X}, y = {current.Y}");
+
+            return true;
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            canvas.DrawPaint(_backgroundPaint);
+
+            foreach (Box box in _boxes)
+            {
+                var left = Math.Min(box.Origin.X, box.Current.X);
+                var right = Math.Max(box.Origin.X, box.Current.X);
+                var top = Math.Max(box.Origin.Y, box.Current.Y);
+                var bottom = Math.Min(box.Origin.Y, box.Current.Y);
+
+                canvas.DrawRect(left, top, right, bottom, _boxPaint);
+            }
+        }
+
+        private void Initialize()
+        {
+            _boxPaint = new Paint();
+            _backgroundPaint = new Paint();
+            _boxPaint.Color = new Color(0x22ff0000);
+            _backgroundPaint.Color = new Color(0xf8efe0);
+        }
+    }
+}
---
+++ DragAndDrawXamarin/Fragments/DragAndDrawFragment.cs
@@ -0,0 +1,21 @@
+﻿using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+
+namespace DragAndDrawXamarin.Fragments
+{
+    public class DragAndDrawFragment : Fragment
+    {
+        public static DragAndDrawFragment NewInstance()
+        {
+            return new DragAndDrawFragment();
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.fragment_drag_and_draw, container, false);
+
+            return view;
+        }
+    }
+}
